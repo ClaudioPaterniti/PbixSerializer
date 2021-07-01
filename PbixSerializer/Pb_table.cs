266c6e55171cfe29c5a4fecb507ed5b27b85a54c
Claudio Paterniti <claudio.paterniti@absontheweb.onmicrosoft.com>
2021-07-01 14:22:35 +0200

    Add project files.

---
+++ PbixSerializer/Pb_table.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AnalysisServices.Tabular;
+using System.Collections.Generic;
+
+namespace PbixSerializer
+{
+    class Pb_partition
+    {
+        public string name;
+        public string type;
+        public string[] expression;
+
+        public Pb_partition(Partition p)
+        {
+            this.name = p.Name;
+            this.type = p.SourceType.ToString();
+            switch (this.type)
+            {
+                case "M":
+                    MPartitionSource mPartition = (MPartitionSource)p.Source;
+                    this.expression = mPartition.Expression.Split('\n');
+                    break;
+                case "Calculated":
+                    CalculatedPartitionSource cPartition = (CalculatedPartitionSource)p.Source;
+                    this.expression = cPartition.Expression.Split('\n');
+                    break;
+                case "Entity":
+                    EntityPartitionSource ePartition = (EntityPartitionSource)p.Source;
+                    this.expression = ePartition.ExpressionSource.Expression.Split('\n');
+                    break;
+                case "Query":
+                    QueryPartitionSource qPartition = (QueryPartitionSource)p.Source;
+                    this.expression = qPartition.Query.Split('\n');
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    class Pb_table
+    {
+        public string name;
+        public List<Pb_partition> partitions;
+        public Pb_table(Table t)
+        {
+            this.name = t.Name;
+            this.partitions = new List<Pb_partition>();
+            foreach (Partition p in t.Partitions)
+                this.partitions.Add(new Pb_partition(p));
+        }
+    }
+}
---
+++ PbixSerializer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AnalysisServices.Tabular;
+using Microsoft.AnalysisServices.AdomdClient;
+using Newtonsoft.Json;
+using JsonSerializer = Microsoft.AnalysisServices.Tabular.JsonSerializer;
+
+namespace PbixSerializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string server_port = args[0];
+            string database_name = args[1];
+            Console.WriteLine($"Server port: {server_port}, Database: {database_name}");
+            string ConnectionString = "DataSource=" + server_port;
+            
+            using (Server server = new Server())
+            {
+                try
+                {
+                    server.Connect(ConnectionString);
+                    var db = server.Databases[database_name];
+                    Pb_table t = new Pb_table(db.Model.Tables.First());
+                    string fileName = "WeatherForecast.json";
+                    string j = JsonConvert.SerializeObject(t, Formatting.Indented);
+                    File.WriteAllText(fileName, j);
+                }
+                catch (Exception ex)
+                {
+                    //log exceptions somewhere
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            Console.WriteLine("Press Enter to close this console window.");
+            Console.ReadLine();
+        }
+    }
+}
